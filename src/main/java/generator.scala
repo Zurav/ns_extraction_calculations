94cc876ba503c2e2e7678178bbcba7dcf56f1557
Zurab <zurab.bzhalava@ki.se>
2017-06-13 09:52:58 +0200

    commit

--- src/main/java/generator.scala
+++ src/main/java/generator.scala
@@ -106,15 +106,26 @@ class generator (db_file:String) {
 
   def generator_X_year(file_location: String) = {
     val column_list = (unique_year_sorted.min to unique_year_sorted.max).toList
-    val column_names = "Ages" :: column_list
+    val full_column_names = "Ages" :: column_list
     val ages = List((25,29),(30,34),(35,39),(40,44),(45,49),(50,54),(55,59),(30,59))
-    val row_names = List("25-29","30-34","35-39","40-44","45-49","50-54","55-59", "All ages: 30-59 years")
+    val full_row_names = List("25-29","30-34","35-39","40-44","45-49","50-54","55-59", "All ages: 30-59 years")
 
 
-    val lists_from_sql = unique_year.map(year => ages.map(age => sql_command_indicators(1, age, year)))
+    val lists_from_sql = unique_year.map(year => ages.map(age => sql_command_indicators(10, age, year)))
+
+    //val tr_lists_from_sql = lists_from_sql.transpose
     val cleaned_lists_from_sql = for (ls <- lists_from_sql if ls.reduceLeft(_ + _) > 0 ) yield ls
-    println(cleaned_lists_from_sql)
+    println(cleaned_lists_from_sql.length)
+    val transposed_lists = cleaned_lists_from_sql.transpose
+    val year_data = for (ls <- transposed_lists if ls.reduceLeft(_ + _) > 0 ) yield ls
 
+    val n_exc_rows = transposed_lists.length - year_data.length
+    val row_names = full_row_names.drop(n_exc_rows)
+    val column_names = full_column_names.take(cleaned_lists_from_sql.length + 1)
+
+    val year_data_row_names = for ((ls, index) <- year_data.zipWithIndex) yield row_names(index) :: ls
+    val ready_to_write = column_names :: year_data_row_names
+    ready_to_write.foreach(x => println(x.mkString(",")))
 
 
   }
